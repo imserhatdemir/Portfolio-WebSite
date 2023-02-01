9438fda5a77197db43f77a1cd390a82ab527d5e1
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2023-02-01 18:50:43 +0300

    Admin 101

---
+++ Controllers/SkillController.cs
@@ -0,0 +1,20 @@
+﻿using BusinnessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager = new SkillManager(new EFSkillDAL());
+        public IActionResult Index()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+    }
+}
