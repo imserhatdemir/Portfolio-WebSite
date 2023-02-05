8c77cb563db5513d69b4f4490bcdb39bd8d2a84a
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2023-02-06 01:22:35 +0300

    admin-service

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,45 @@
+﻿using BusinnessLayer.Concrete;
+using BusinnessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EFAboutDAL());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var value = aboutManager.GetById(1);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult Index(About s)
+        {
+            AboutValidator validationRules = new AboutValidator();
+            ValidationResult result = validationRules.Validate(s);
+            if (result.IsValid)
+            {
+                aboutManager.TUpdate(s);
+                return RedirectToAction("Index","Default");
+            }
+            else
+            {
+                foreach(var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,85 @@
+﻿using BusinnessLayer.Concrete;
+using BusinnessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EFServiceDAL());
+        public IActionResult Index()
+        {
+            var value = serviceManager.TGetList();
+            return View(value);
+        }
+
+
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddService(Service s)
+        {
+            ServiceValidator validationRules = new ServiceValidator();
+            ValidationResult result = validationRules.Validate(s);
+            if (result.IsValid)
+            {
+                serviceManager.TAdd(s);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public IActionResult DeleteService(int id)
+        {
+            var value = serviceManager.GetById(id);
+            serviceManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            var value = serviceManager.GetById(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult EditService(Service s)
+        {
+            ServiceValidator validationRules = new ServiceValidator();
+            ValidationResult result = validationRules.Validate(s);
+            if (result.IsValid)
+            {
+                serviceManager.TUpdate(s);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
