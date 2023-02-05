5df05323e15bc422917f1139e2df10dd15a0fff9
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2023-02-05 04:51:45 +0300

    update

--- Controllers/FeatureController.cs
+++ Controllers/FeatureController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinnessLayer.Concrete;
+using BusinnessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +13,34 @@ namespace Portfolio.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EFFeatureDAL());
+
+        [HttpGet]
         public IActionResult Index()
         {
+            var values = featureManager.GetById(1);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Feature s)
+        {
+            FeatureValidator validationRules = new FeatureValidator();
+            ValidationResult result = validationRules.Validate(s);
+            if (result.IsValid)
+            {
+                featureManager.TUpdate(s);
+                return RedirectToAction("Index","Default");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
+            
         }
     }
 }
