d991f3677be1dfadb15de7032038cb8fb32ee035
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2023-03-21 23:41:21 +0300

    login

---
+++ Areas/Writer/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Areas.Writer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class LoginController : Controller
+    {
+
+        private readonly SignInManager<WriterUser> _userManager;
+
+        public LoginController(SignInManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserSignViewModel s)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userManager.PasswordSignInAsync(s.UserName, s.Password, true, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Default");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı Kullanıcı adı veya şifre");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Areas/Writer/Models/UserSignViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Areas.Writer.Models
+{
+    public class UserSignViewModel
+    {
+        [Display(Name ="Kullanıcı Adı")]
+        [Required(ErrorMessage = "Kullanıcı Adı girin")]
+        public string UserName { get; set; }
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage =  "Şifre girin")]
+        public string Password { get; set; }
+    }
+}
