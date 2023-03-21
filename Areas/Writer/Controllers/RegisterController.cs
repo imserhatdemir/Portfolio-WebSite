24975a0c67198bb9e8383901db250dcd44ffb74a
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2023-03-21 22:47:02 +0300

    register and areas

---
+++ Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,65 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Areas.Writer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public RegisterController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new UserRegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserRegisterViewModel d)
+        {
+            if (ModelState.IsValid)
+            {
+                WriterUser w = new WriterUser()
+                {
+                    Name = d.Name,
+                    Surname = d.SurName,
+                    Email = d.Mail,
+                    UserName = d.UserName,
+                    Image = d.ImageURL
+                };
+
+                if (d.Password == d.ConfirmPassword)
+                {
+                    var result = await _userManager.CreateAsync(w, d.Password);
+
+
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Login");
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
+                    }
+                }
+            }
+           
+            return View();
+        }
+    }
+}
---
+++ Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Lütfen adınızı giriniz")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen soyadınızı giriniz")]
+        public string SurName { get; set; }
+        [Required(ErrorMessage ="Lütfen kullanıcı adını giriniz")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Lütfen şifre giriniz")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Lütfen şifrenizi tekrar giriniz")]
+        [Compare("Password",ErrorMessage ="Şifreler uyumlu değil")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Lütfen mail giriniz")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Lütfen görsel giriniz")]
+        public string ImageURL { get; set; }
+    }
+}
